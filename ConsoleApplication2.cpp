a33050bf28ebd828d51beb1641adda3aeaae4bf1
qeuete <mkkkk_21@mail.ru>
2024-01-30 14:24:45 +0300

    13

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int a;
+	int b;
+
+	cout << "Число а=";
+	cin >> a;
+	cout << "Число b=";
+	cin >> b;
+
+	int* p_a = &a;
+	int* p_b = &b;
+
+	*p_a *= 2;
+	*p_b /= 2;
+
+	cout << "p_a * 2 =" << *p_a << endl;
+	cout << "адрес p_a * 2 =" << p_a << endl;
+	cout << "p_b / 2 =" << *p_b << endl;
+	cout << "адрес p_b / 2 =" << p_b << endl;
+
+	system("pause");
+	return 0;
+}
